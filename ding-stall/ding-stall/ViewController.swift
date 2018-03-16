7000ef8c85d1adbc7882faef6bf863cde15b93ad
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-16 12:52:32 +0800

    Add firebase for ding! stall

---
+++ ding-stall/ding-stall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ding-stall
+//
+//  Created by Yunpeng Niu on 16/03/18.
+//  Copyright © 2018年 CS3217 Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
