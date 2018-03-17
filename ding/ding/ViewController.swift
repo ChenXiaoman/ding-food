5a9cad88f11fe5c9b00f8bd3162a9d642474b620
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-17 11:41:25 +0800

    Add new pod for firebase realtime database

--- ding/ding/ViewController.swift
+++ ding/ding/ViewController.swift
@@ -12,10 +12,5 @@ import Firebase
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let ref = Database.database().reference()
-        ref.child("weather").observeSingleEvent(of: .value, with: { snapshot in
-            print(snapshot.value ?? 0)
-        })
     }
 }
