163e16cb7c1be7f6472ccadeb745c8e9a2344d0f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-18 11:47:06 +0800

    Add Authorizer

---
+++ ding-stall/ding-stall/Others/Database.swift
@@ -0,0 +1,21 @@
+//
+//  Database.swift
+//  ding-stall
+//
+//  Created by Yunpeng Niu on 18/03/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
+//
+
+import FirebaseDatabase
+
+/**
+ This class acts as an abstraction layer between the server and client. It
+ provides basic functionalities regarding data interaction with the server
+ database.
+
+ - Author: Group 3 @ CS3217
+ - Date: March 2018
+ */
+class Database {
+    
+}
