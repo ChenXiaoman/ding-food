90e6b47f59894dd9e85ea273b075e4de525c8e3b
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-18 12:57:00 +0800

    Add observe value to storage class

--- ding-stall/ding-stall/Others/Storage.swift
+++ ding-stall/ding-stall/Others/Storage.swift
@@ -17,5 +17,15 @@ import FirebaseDatabase
  - Date: March 2018
  */
 class Storage {
+    /// A reference to the Firebase realtime database, as access point to the server.
     private static let ref = Database.database().reference()
+
+    /// Observes the value at a certain path. The handler will be notified each time the data
+    /// at the specified path (and its child nodes) change.
+    /// - Parameters:
+    ///    - path: The path to the observed data.
+    ///    - handler: The handler being called whenever the data at the specified path changes.
+    func observeValue(of path: String, onChange handler: @escaping (DataSnapshot) -> Void) {
+        Storage.ref.child(path).observe(.value, with: handler)
+    }
 }
