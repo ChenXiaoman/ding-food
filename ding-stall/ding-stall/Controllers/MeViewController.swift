996d9a737cb6e7fa4c44792290df2bd3e184618e
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-23 21:47:28 +0800

    Add table view

--- ding-stall/ding-stall/Controllers/MeViewController.swift
+++ ding-stall/ding-stall/Controllers/MeViewController.swift
@@ -15,8 +15,67 @@ import UIKit
  - Date: March 2018
  */
 class MeViewController: UIViewController {
+    /// The table view to use as the setting menu
+    @IBOutlet private weak var settingMenu: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        settingMenu.delegate = self
+        settingMenu.dataSource = self
+        settingMenu.tableFooterView = UIView()
+    }
+}
+
+/**
+ Extension for `MeViewController` so that it can manage `UITableView`.
+ */
+extension MeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingMenuInfo.count
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constants.meSettingCellHeight
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.meSettingCellId,
+                                                       for: indexPath) as? MeSettingMenuCell,
+            let info = SettingMenuInfo(rawValue: indexPath.row) else {
+            fatalError("Unable to get a new cell.")
+        }
+        cell.setName(info.name)
+        cell.isDangerous = info.isDangerous
+
+        return cell
+    }
+}
+
+/**
+ Contains information about the cells in setting menu of me scene.
+ */
+enum SettingMenuInfo: Int {
+    case history
+    case profile
+    case setting
+    case about
+    case logout
+
+    /// The number of cells.
+    static let count = 5
+    /// The labels of all cells.
+    static let labels = ["Order History", "My Profile", "Settings", "About", "Log Out"]
+    /// Indicates whether it is dangerous.
+    static let isDangerous = [false, false, false, false, true]
+
+    /// The name of a certain setting menu cell.
+    var name: String {
+        return SettingMenuInfo.labels[rawValue]
+    }
+
+    /// Indicates whether a certain setting menu cell is dangerous.
+    var isDangerous: Bool {
+        return SettingMenuInfo.isDangerous[rawValue]
     }
 }
--- ding-stall/ding-stall/Others/Constants.swift
+++ ding-stall/ding-stall/Others/Constants.swift
@@ -22,12 +22,8 @@ class Constants {
     /// The height of the current device's screen.
     static let screenHeight = UIScreen.main.bounds.height
 
-    /// The border width for `SettingMenuCell`.
-    static let settingMenuCellBorderWidth = CGFloat(0.5)
-    /// The suggested height for `SettingMenuCell`.
-    static let settingMenuCellHeight = CGFloat(50)
-    /// The suggested font size for `SettingMenuCell`.
-    static let settingMenuCellFontSize = CGFloat(20)
-    /// The suggested inset for `SettingMenuCell`.
-    static let settingMenuCellInset = CGFloat(5)
+    /// The identifier for me setting menu cells.
+    static let meSettingCellId = "meSettingMenuCell"
+    /// The height for me setting menu cells.
+    static let meSettingCellHeight = CGFloat(60)
 }
