5f295dae1d5e94b26bc6c59b0696e24ee84a7d66
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-23 18:29:52 +0800

    Save progress

--- ding-stall/ding-stall/Controllers/LoginViewController.swift
+++ ding-stall/ding-stall/Controllers/LoginViewController.swift
@@ -16,21 +16,39 @@ import FirebaseAuthUI
  - Author: Group 3 @ CS3217
  - Date: March 2018
  */
-class LoginViewController: UIViewController, FUIAuthDelegate {
+class LoginViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         guard Auth.auth().currentUser != nil else {
             loadLoginView(animated)
             return
         }
+        loadTabBarView(animated)
     }
 
-    func loadLoginView(_ animated: Bool) {
+    /// Loads the login view from Firebase Auth UI library.
+    /// - Parameter animated: If true, the view was added to the window using an animation.
+    private func loadLoginView(_ animated: Bool) {
         if let authUI = FUIAuth.defaultAuthUI() {
             authUI.delegate = self
             present(authUI.authViewController(), animated: animated, completion: nil)
         }
     }
 
+    /// Loads the main tab bar view from storyboard.
+    /// - Parameter animated: If true, the view was added to the window using an animation.
+    private func loadTabBarView(_ animated: Bool) {
+        let id = Constants.tabBarControllerId
+        guard let tabBarController = storyboard?.instantiateViewController(withIdentifier: id) else {
+            fatalError("Could not find the controller for main tab bar")
+        }
+        navigationController?.pushViewController(tabBarController, animated: animated)
+    }
+}
+
+/**
+ Extension for `LoginViewController` to act as a delegate for Firebase Auth UI.
+ */
+extension LoginViewController: FUIAuthDelegate {
     func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
         guard let user = authDataResult?.user else {
             return
---
+++ ding-stall/ding-stall/Others/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ding-stall
+//
+//  Created by Yunpeng Niu on 23/03/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
+//
+
+class Constants {
+    static let tabBarControllerId = "mainTabBar"
+}
