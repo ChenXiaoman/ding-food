c5ed8de5f2a5dd6b9e7c7d6c3c3e453408ae3008
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-03-23 21:05:22 +0800

    Save progress

--- ding-stall/ding-stall/Others/Constants.swift
+++ ding-stall/ding-stall/Others/Constants.swift
@@ -21,4 +21,13 @@ class Constants {
     static let screenWidth = UIScreen.main.bounds.width
     /// The height of the current device's screen.
     static let screenHeight = UIScreen.main.bounds.height
+
+    /// The border width for `SettingMenuCell`.
+    static let settingMenuCellBorderWidth = CGFloat(0.5)
+    /// The suggested height for `SettingMenuCell`.
+    static let settingMenuCellHeight = CGFloat(50)
+    /// The suggested font size for `SettingMenuCell`.
+    static let settingMenuCellFontSize = CGFloat(20)
+    /// The suggested inset for `SettingMenuCell`.
+    static let settingMenuCellInset = CGFloat(5)
 }
