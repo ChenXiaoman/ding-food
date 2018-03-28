abda0becbe3296131727dedf1967e8e686ac7d94
江春辉 <jiangchunhui007@gmail.com>
2018-03-28 11:34:58 +0800

    update the FirebaseObject

--- ding-stall/ding-stall/Model/Protocols.swift
+++ ding-stall/ding-stall/Model/Protocols.swift
@@ -1,24 +1,70 @@
 //
-//  Protocols.swift
-//  DingPod
+//  FirebaseObject.swift
+//  ding
 //
-//  Created by Jiang Chunhui on 17/03/18.
-//  Copyright © 2018 JCH. All rights reserved.
+//  Created by Yunpeng Niu on 27/03/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
 //
 
-import Foundation
-import Firebase
+import FirebaseDatabase
 
-/// Represents an object stored in Firebase database
+/**
+ Each object that would be stored in Firebase database is a `FirebaseObject`. This protocol
+ provides the basic capability, such as serialization.
+
+ `FirebaseObject` conforms to `Codable` protocol. This is because we can think of Firebase
+ database as a cloud-hosted JSON tree. Thus, it would be nice if we could use `Codable` types,
+ which can be easily converted from/to JSON objects.
+
+ `FirebaseObject` conforms to `Hashable` protocol. This is because each JSON node in Firebase
+ database has a key, which should be unique. Thus, a certain `FirebaseObject` is identifiable
+ by its key value, the `id` attribute.
+
+ - Author: Group 3 @ CS3217
+ - Date: March 2018
+ */
 public protocol FirebaseObject: Codable, Hashable {
+    /// The identifier of a `FirebaseObject`, which must be unique among all homogenous
+    /// objects. For instance, among all posts (which are stored at the path "/posts"
+    /// in Firebase), their `id`s must all be unique.
+    ///
+    /// The underlying reason is that, a JSON object does not allow duplicate keys (since
+    /// we can think of the Firebase database as a cloud-hosted JSON tree).
     var id: String { get }
 
+    /// The path of a `FirebaseObject` type, at which all homogenous objects of this type
+    /// would be stored under in Firebase.
+    ///
+    /// For instance, if we want to create blog systems, the path for `Post` type should
+    /// be "/posts". Notice that the path should be a string with a "/" prefix but without
+    /// a "/" suffix.
+    ///
+    /// We suggest the value of `path` should be a descriptive English word, usually in its
+    /// plural form.
     static var path: String { get }
 }
 
+/**
+ Extension for `FirebaseObject`, which provides some utility methods ready for use.
+ */
 extension FirebaseObject {
+    /// The hash value of a `FirebaseObject`.
     public var hashValue: Int {
         return id.hashValue
+    }
+
+    /// Two `FirebaseObject`s are equal as long as their identifiers are equal.
+    /// - Parameters:
+    ///    - lhs: The first object to compare.
+    ///    - rhs: The second object to compare.
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    /// Gets a unique `id` for this `FirebaseObject`. This property could be useful
+    /// when you create a new object locally.
+    public static var getAutoId: String {
+        return DatabaseRef.getAutoId(of: Self.path)
     }
 
     /// Converts a given `FirebaseObject` into a dictionary format so that it can be
@@ -32,7 +78,7 @@ extension FirebaseObject {
             let data = try? JSONEncoder().encode(self),
             let obj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments),
             var dict = obj as? [String: Any] else {
-            return [:]
+                return [:]
         }
         dict["id"] = nil
         return dict
@@ -58,17 +104,13 @@ extension FirebaseObject {
         return obj
     }
 
-    /// Generate an id for a fireBase object
-    public static func getAutoId() -> String {
-        return Storage.getAutoId(of: path)
-    }
-
+    /// Saves this `FirebaseObject` to the Firebase database.
+    ///
+    /// Notice: It is possible to override the `CodingKeys` attribute if you do not want
+    /// to save all attributes to the database or want to customize the key name for
+    /// certain attributes.
     public func save() {
-        let storage = Storage()
-        storage.setChildNode(of: Self.path, to: self)
-    }
-
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.id == rhs.id
+        DatabaseRef.setChildNode(of: Self.path, to: self)
     }
 }
+
