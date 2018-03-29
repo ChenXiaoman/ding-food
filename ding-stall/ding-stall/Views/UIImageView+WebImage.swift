61cf70c4818bf788d7bda9fcc4e1f60be29a84c6
江春辉 <jiangchunhui007@gmail.com>
2018-03-29 12:06:19 +0800

    add the image backend file

---
+++ ding-stall/ding-stall/Views/UIImageView+WebImage.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+WebImage.swift
+//  ding
+//
+//  Created by Yunpeng Niu on 28/03/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
+//
+
+import FirebaseStorageUI
+
+/**
+ Extension for `UIImageView` to provide some utility methods for web images.
+ */
+extension UIImageView {
+    /// Sets the content of a certain image view to be from web.
+    /// - Parameters:
+    ///    - path: The path at which the web image is stored.
+    ///    - placeholder: The placeholder image to be shown before the image is loaded,
+    /// whose default value is `nil`.
+    func setWebImage(at path: String, placeholder: UIImage? = nil) {
+        sd_setImage(with: StorageRef.getNodeRef(of: path), placeholderImage: placeholder)
+    }
+}
