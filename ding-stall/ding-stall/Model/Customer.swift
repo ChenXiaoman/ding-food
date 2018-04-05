74bd1c6b77241ac54e42769338f56b1e6fde6ae5
江春辉 <jiangchunhui007@gmail.com>
2018-04-05 13:10:29 +0800

    add the customer model

---
+++ ding-stall/ding-stall/Model/Customer.swift
@@ -0,0 +1,19 @@
+//
+//  Customer.swift
+//  DingPod
+//
+//  Created by Jiang Chunhui on 17/03/18.
+//  Copyright © 2018 JCH. All rights reserved.
+//
+
+/**
+ Represents a registered user in the application.
+ */
+public struct Customer: FirebaseObject {
+    public static let path = "/customer_profiles"
+
+    /// The customer's UID.
+    public let id: String
+    /// The customer's real name.
+    public let name: String
+}
