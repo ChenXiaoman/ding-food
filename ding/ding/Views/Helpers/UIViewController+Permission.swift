4992aaf039406a6b408feeae4530bee106d4d3b6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-06 15:53:19 +0800

    Performs some refactoring

--- ding/ding/Views/Helpers/UIViewController+Permission.swift
+++ ding/ding/Views/Helpers/UIViewController+Permission.swift
@@ -18,33 +18,52 @@ import UIKit
 extension UIViewController {
     /// Used to handle all logics related to Firebase Auth.
     private static let currentAuthorizer = Authorizer()
+    /// The title for alert box.
+    private static let alertTitle = "Oops"
+    /// The message to require login.
+    private static let loginMessage = "Please log in first to proceed."
+    /// The message to require email verification.
+    private static let verifyEmailMessage = "Please verify your email address and then log in back."
+    /// The tab bar index for me view.
+    private static let meViewTabBarIndex = 2
 
     /// An adapter to access authorizer object.
     var authorizer: Authorizer {
         return UIViewController.currentAuthorizer
     }
 
-    /// Pop up warning when the user is not logged in or
-    /// the account is not verify
-    private func handleUserNotLogin() {
-        // stopLoading()
-        if authorizer.didLogin {
-            popUpNeedToLogin()
+    /// Performs permission control and decides whether to allow the user to
+    /// access the ongoing order listing.
+    /// - Returns: true if the permission check passes; false otherwise.
+    func checkPermission() -> Bool {
+        guard authorizer.didLogin else {
+            alertLoginRequired()
+            return false
         }
-        if authorizer.isEmailVerified {
-            popUpNeedToVerityEmail()
+        guard authorizer.isEmailVerified else {
+            alertVerifyEmail()
+            return false
         }
+        return true
     }
 
-    /// Pop up an alert warning user to verify email
-    func popUpNeedToVerityEmail() {
-        DialogHelpers.showAlertMessage(in: self, title: "Oops",
-                                       message: "Please verify your account in email in order to check your orders")
+    /// Shows an alert message to ask the user to log in first and brings the user to login.
+    private func alertLoginRequired() {
+        DialogHelpers.showAlertMessage(in: self, title: UIViewController.alertTitle,
+                                       message: UIViewController.loginMessage,
+                                       onConfirm: toMeTabBarView)
     }
 
-    /// Pop up an alert warning user to log in first
-    func popUpNeedToLogin() {
-        DialogHelpers.showAlertMessage(in: self, title: "Oops",
-                                       message: "Please log in first in order to check your orders")
+    /// Shows an alert message to ask the user to verify the account  and pops to the
+    /// login view.
+    private func alertVerifyEmail() {
+        DialogHelpers.showAlertMessage(in: self, title: UIViewController.alertTitle,
+                                       message: UIViewController.verifyEmailMessage,
+                                       onConfirm: toMeTabBarView)
+    }
+
+    /// Brings the user to the me view (i.e, the last index in the tab bar).
+    private func toMeTabBarView() {
+        tabBarController?.selectedIndex = UIViewController.meViewTabBarIndex
     }
 }
