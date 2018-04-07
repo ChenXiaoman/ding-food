9bdd9b6776dfa639cca0e3952d3a0fe5261793ec
Chen Xiaoman <836932889@qq.com>
2018-04-07 19:22:44 +0800

    Hide review section

--- ding/ding/Controllers/Order/OrderDetailViewController.swift
+++ ding/ding/Controllers/Order/OrderDetailViewController.swift
@@ -9,6 +9,10 @@
 import FirebaseDatabaseUI
 
 class OrderDetailViewController: UIViewController {
+    /// The UIView for displaying order information
+    @IBOutlet weak private var orderView: OrderView!
+    /// The UIView for review section.
+    @IBOutlet weak private var reviewView: ReivewUIView!
     /// Table view for displaying list of food.
     @IBOutlet weak private var foodTableView: UITableView!
     /// The 'Order' object which the view controller is displaying.
@@ -17,5 +21,22 @@ class OrderDetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         // Shows the navigation bar
         navigationController?.setNavigationBarHidden(false, animated: animated)
-    }    
+        
+        hideOrShowReview()
+    }
+    
+    /// Checks whether an order is ready for review. An order is ready
+    /// for review only if the status is collected.
+    /// If no, hides the review section because review is not available.
+    /// at this time. If yes, shows the review section.
+    private func hideOrShowReview() {
+        guard let order = order else {
+            return
+        }
+        if order.status == OrderStatus.collected {
+            reviewView.isHidden = false
+        } else {
+            reviewView.isHidden = true
+        }
+    }
 }
---
+++ ding/ding/Views/Order/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  ding
+//
+//  Created by Chen Xiaoman on 7/4/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
+//
+
+import UIKit
+
+class OrderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
