21f4626e36cde00e8c0964ab1f3098afe8d568ca
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-07 11:56:40 +0800

    Add publicity page

---
+++ ding/ding/Model/Search/Filter.swift
@@ -0,0 +1,17 @@
+//
+//  FilterIdentifier.swift
+//  ding
+//
+//  Created by Calvin Tantio on 17/3/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
+//
+
+/**
+ Represents an identifier that a user can use to filter a stall by.
+ */
+public struct Filter: FirebaseObject {
+    public static let path = "/filters"
+
+    // This id is the name of the filter
+    public let id: String
+}
