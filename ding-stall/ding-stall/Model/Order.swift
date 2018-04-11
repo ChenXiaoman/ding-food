7c12352713afc7f1cbc36173b6c8746a8e2ecbc6
江春辉 <jiangchunhui007@gmail.com>
2018-04-11 11:47:57 +0800

    modify order model

--- ding-stall/ding-stall/Model/Order.swift
+++ ding-stall/ding-stall/Model/Order.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 /**
- Represents an order posted by a user. An order may have various food with different
- quantities from the same stall.
+ `Order` represents a collection of food with certain quantities. An `Order` is
+ submitted by the customer and confirmed by the stall owner.
+
+ - Author: Group 3 @ CS3217
+ - Date: March 2018
  */
 public struct Order: FirebaseObject {
     public static let path = "/orders"
@@ -39,26 +42,39 @@ public struct Order: FirebaseObject {
     /// A mapping from food id to its human-readable name (to apply fan-out & denormalization
     /// pattern here).
     let foodName: [String: String]
+    /// A mapping from food id to the
+    /// mapping of option to user's choice for the option
+    let options: [String: [String: String]]
     /// A pre-computed total price to improve efficiency. Another consideration is that the
     /// total price should not be affected by changes to prices after the order is created.
     let totalPrice: Double
 
-    init(status: OrderStatus = .preparing, review: Review? = nil, stall: StallOverview, food: [Food: Int]) {
+    init(status: OrderStatus = .preparing, review: Review? = nil, stall: StallOverview, food: [Food: Int], options: [Food: [String: String]]) {
         id = Order.getAutoId
         self.status = status
         self.review = review
         customerId = Order.authorizer.userId
         stallId = stall.id
         createdAt = Date()
+
+        // Transform food object in dictionary to food id
         var foodQuantity: [String: Int] = [:]
         var foodName: [String: String] = [:]
         food.forEach { key, value in
             foodQuantity[key.id] = value
             foodName[key.id] = key.name
         }
+
+        // Transform food object  in dictionary to food id
+        var foodOptions: [String: [String: String]] = [:]
+        options.forEach { key, value in
+            foodOptions[key.id] = value
+        }
+
         self.foodQuantity = foodQuantity
         self.foodName = foodName
-        totalPrice = food.reduce(0.0) { sum, now in
+        self.options = foodOptions
+        self.totalPrice = food.reduce(0.0) { sum, now in
             sum + now.key.price * Double(now.value)
         }
     }
@@ -70,7 +86,6 @@ public struct Order: FirebaseObject {
             return accum + String(format: Order.foodDescriptionFormat, current.value, name)
         }
     }
-
 }
 
 public enum OrderStatus: String, Codable {
@@ -85,3 +100,4 @@ public enum OrderStatus: String, Codable {
         return self == .preparing || self == .ready
     }
 }
+
