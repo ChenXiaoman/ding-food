c5d8d80c9b6196c8c10b355c3f11d882add18547
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-14 16:32:48 +0800

    Add missing files

---
+++ ding/ding/Views/Order/ReviewTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReviewTableViewCell.swift
+//  ding
+//
+//  Created by Chen Xiaoman on 25/3/18.
+//  Copyright © 2018 CS3217 Ding. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    public static let tableViewIdentifier = "ReviewTableViewCell"
+
+}
